dc806568b5af483b49ad8e3dd3136bdda4825a82
DavidM839 <107301911+DavidM839@users.noreply.github.com>
2023-11-09 18:52:55 -0600

    Separacion vistas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React from 'react';
 import './App.css';
 import Cliente from './componentes/Cliente';
 import Cuenta from './componentes/Cuenta';
 import Transaccion from './componentes/Transaccion';
 import GraficoDepositos from './componentes/GraficoDepositos';
 import GraficoRetiros from './componentes/GraficoRetiros';
+import Navbar from './componentes/Navbar';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-     
-        <Cliente/>
-       <Cuenta/>
-       <Transaccion/>
-       <GraficoDepositos/>
-       <GraficoRetiros/>
-    </div>
-  );
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/Cliente" element={<Cliente />} />
+      <Route path="/Cuenta" element={<Cuenta />} />
+      <Route path="/Transaccion" element={<Transaccion />} />
+      <Route path="/GraficoDepositos" element={<GraficoDepositos />} />
+      <Route path="/GraficoRetiros" element={<GraficoRetiros />} />
+    </Routes>
+  </Router>
+);
 }
 
 export default App;
